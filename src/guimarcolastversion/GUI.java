01c9d0858fd0b99ffb4f4d5c27a99f6392fdfa9f
marco <marco@marco-Inspiron-5570>
2018-11-12 15:26:23 +0100

    creo il progetto

---
+++ src/guimarcolastversion/GUI.java
@@ -0,0 +1,18 @@
+package guimarcolastversion;
+
+import javax.swing.JPanel;
+
+/**
+ * @author Marco Forlin, Matteo faveri
+ */
+public interface GUI {
+    
+    // Colora di bianco i bordi delle label del pannello passato
+    public void whiteLabels(JPanel panel);
+    
+    // Refresh del pannello
+    public void refreshPanel(JPanel panel);
+    
+    // Visualizza solo il pannello passato
+    public void closeAllExcept(JPanel p);    
+}
