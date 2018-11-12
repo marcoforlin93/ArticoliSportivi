3160b40df8580948a9ebd598b233f06706cfdafd
matteo <matteo@jarvis>
2018-11-12 15:42:01 +0100

    prima modifica

---
+++ src/gui/eliminare.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author matteo
+ */
+public class eliminare {
+    int a;
+
+    public eliminare(int a) {
+    this.a = a;
+    }
+    
+    public int f1(int p1) {
+        return ++p1;
+    }
+    
+    public boolean f2(int p1) {
+        return p1==1;
+    }
+}
